17932aa68072017f9410e8658816a7a6a51efcac
midaismael <midaismael@gmail.com>
2025-01-21 20:49:43 +0200

    hero section with assets(fonts, images, videos) and itis animation

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AnimatedTitle() {
+  return (
+    <div>
+      <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+        Disc<b>o</b>ver the World's <br />l<b>a</b>rgest shared adventure,
+      </div>
+    </div>
+  );
+}
+
+export default AnimatedTitle;
