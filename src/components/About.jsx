ae968d1df8d71333f1096aeb75b8e1a1d51c1d3a
midaismael <midaismael@gmail.com>
2025-01-21 21:40:23 +0200

    animated title (GSAP)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,7 +30,10 @@ function About() {
         <h2 className="font-general text-sm uppercase md:text-[10px]">
           Welcome to Zentry
         </h2>
-        <AnimatedTitle />
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the World's <br />l<b>a</b>rgest shared adventure,"
+          containerClass="mt-5 !text-black text-center "
+        />
 
         <div className="about-subtext">
           <p>The Game of Games begins-your life, now an epic MMORPG</p>
